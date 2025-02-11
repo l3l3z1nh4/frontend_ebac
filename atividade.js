15d39a25224d2b76c7c2d1e325cfaa67b204f16d
l3l3z1nh4 <148493822+l3l3z1nh4@users.noreply.github.com>
2025-02-11 18:03:05 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,5 @@
+"use strict";
+//FUNÇÃO MULTIPLICAÇÃO
+const multiplica = (v1, v2) => v1 * v2;
+//FUNÇÃO SAUDAÇÃO
+const saudacao = (nome) => `olá ${nome} :)`;
---
+++ atividade.ts
@@ -0,0 +1,6 @@
+
+//FUNÇÃO MULTIPLICAÇÃO
+const multiplica = (v1: number, v2: number) => v1 * v2;
+
+//FUNÇÃO SAUDAÇÃO
+const saudacao = (nome: string) => `olá ${nome} :)`;
